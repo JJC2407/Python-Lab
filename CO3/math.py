217428031e9e29383881cbe5db034da41003b8e9
JJC2407 <joyaljose2407@gmail.com>
2022-03-02 22:37:33 +0530

    python

---
+++ CO3/math.py
@@ -0,0 +1,6 @@
+#26 Joyal Jose
+from math import sqrt, factorial
+n=int(input("Enter a number for finding square root       :  "))
+x=int(input("Enter anthother number for finding factorial : "))
+print("\nSquare root of",n," : ",sqrt(n))
+print("Factorial of ",x,"    : ",factorial(x))
